ecb96c279fdea99229332284c4138f85d438c49c
guillem.cordoba <guillem.cordoba@gmail.com>
2024-11-11 11:45:32 +0100

    Added hashesProperty

--- packages/elements/src/holo-hash-property.ts
+++ packages/elements/src/holo-hash-property.ts
@@ -8,6 +8,13 @@ export function hashState() {
 	};
 }
 
+export function hashesState() {
+	return {
+		hasChanged: (oldVal: HoloHash[], newVal: HoloHash[]) =>
+			oldVal?.toString() !== newVal?.toString(),
+	};
+}
+
 export function hashProperty(
 	attributeName: string,
 ): PropertyDeclaration<object | null, unknown> {
@@ -18,5 +25,21 @@ export function hashProperty(
 			oldVal?.toString() !== newVal?.toString(),
 		converter: (s: string | undefined) =>
 			s && s.length > 0 && decodeHashFromBase64(s),
+	};
+}
+
+export function hashesProperty(
+	attributeName: string,
+): PropertyDeclaration<object | null, unknown> {
+	return {
+		attribute: attributeName,
+		type: Object,
+		hasChanged: (oldVal: HoloHash[], newVal: HoloHash[]) =>
+			oldVal?.toString() !== newVal?.toString(),
+		converter: (s: string | null) => {
+			if (!s) return [];
+			const split = s.split(',');
+			return split.map(decodeHashFromBase64);
+		},
 	};
 }
